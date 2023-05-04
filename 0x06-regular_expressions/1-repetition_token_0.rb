929d020d66bfee0611f25623ef7c19cefc1a8f6a
manyouu <pharaohmanyou00@yahoo.com>
2023-05-04 11:40:23 -0700

    all task

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,19 +1,2 @@
 #!/usr/bin/env ruby
-
-# Ensure an argument is provided
-unless ARGV[0]
-puts "Please provide a string as an argument."
-exit
-end
-
-# Regular expression pattern
-pattern = /hbt{2,5}n/
-
-# Match the argument against the pattern
-matches = ARGV[0].scan(pattern)
-
-if matches.empty?
-puts "No match found."
-else
-puts "Matches found: #{matches.join(', ')}"
-end
+puts ARGV[0].scan(/hbt{2,5}n/).join
